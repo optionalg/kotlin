1cd9cdad3c6a826463b08d28a5231707c88c793a
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2013-11-11 15:52:14 +0400

    Stdlib compilation error fixes with new diagnostics

--- libraries/stdlib/src/kotlin/AssertionsJVM.kt
+++ libraries/stdlib/src/kotlin/AssertionsJVM.kt
@@ -8,7 +8,7 @@ private val ASSERTIONS_ENABLED = _Assertions.javaClass.desiredAssertionStatus()
 * Throws an [[AssertionError]] with an optional *message* if the *value* is false
 * and runtime assertions have been enabled on the JVM using the *-ea* JVM option.
 */
-public inline fun assert(value: Boolean, message: Any = "Assertion failed") {
+public /*inline*/ fun assert(value: Boolean, message: Any = "Assertion failed") {
     if (ASSERTIONS_ENABLED) {
         if (!value) {
             throw AssertionError(message)
@@ -20,7 +20,7 @@ public inline fun assert(value: Boolean, message: Any = "Assertion failed") {
  * Throws an [[AssertionError]] with the specified *lazyMessage* if the *value* is false
  * and runtime assertions have been enabled on the JVM using the *-ea* JVM option.
  */
-public inline fun assert(value: Boolean, lazyMessage: () -> String) {
+public /*inline*/ fun assert(value: Boolean, lazyMessage: () -> String) {
     if (ASSERTIONS_ENABLED) {
         if (!value) {
             val message = lazyMessage()
