cd58b711ea18af8846c6622fdbcb6e5a0cd68639
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2013-11-19 15:02:36 +0400

    Nothing to inline diagnostic

--- compiler/testData/diagnostics/tests/inline/invoke.kt
+++ compiler/testData/diagnostics/tests/inline/invoke.kt
@@ -6,11 +6,11 @@ inline fun inlineFunWithInvoke(s: (p: Int) -> Unit) {
     s invoke 11
 }
 
-inline fun inlineFunWithInvokeNonInline(noinline s: (p: Int) -> Unit) {
+<!NOTHING_TO_INLINE!>inline fun inlineFunWithInvokeNonInline(noinline s: (p: Int) -> Unit) {
     s(11)
     s.invoke(11)
     s invoke 11
-}
+}<!>
 
 inline fun Function1<Int, Unit>.inlineExt() {
     invoke(11)
